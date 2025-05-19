bc094919147a93c4fb06665b17c27c94edccbe91
Matěj Brachtl <147615009+M-Brachtl@users.noreply.github.com>
2025-05-19 08:56:50 +0000

    For loop for matches

--- tipovacka/src/App.vue
+++ tipovacka/src/App.vue
@@ -2,13 +2,19 @@
 //import { ref } from 'vue';
 import MatchItem from './components/MatchItem.vue';
 
+const matches = [
+  { match: 'Czechia vs Switzerland', result: '2:1', guess: '2:0' },
+  { match: 'Germany vs France', result: '1:1', guess: '1:1' },
+  { match: 'Italy vs Spain', result: '0:2', guess: '0:3' }
+];
+
 </script>
 
 <template>
   <header>Tipovačka</header>
   <main>
     <div id="heading"><span>Zápasy</span><span>Výsledky</span><span>Tipy</span><span>Body</span></div>
-    <MatchItem match="Czechia vs Switzerland" result="2:1" guess="2:0"/>
+    <MatchItem v-for="(item, index) in matches" :key="index" :match="item.match" :result="item.result" :guess="item.guess" />
   </main>
 </template>
 
